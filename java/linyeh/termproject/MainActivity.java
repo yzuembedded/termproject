178a7fd2410f67352247762013e6bbde45f07a40
fbi369123456 <fbi36960207@gmail.com>
2016-12-10 15:45:34 +0800

    GG

--- java/linyeh/termproject/MainActivity.java
+++ java/linyeh/termproject/MainActivity.java
@@ -1,13 +1,45 @@
 package linyeh.termproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.*;
+
 import android.os.Bundle;
+import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private String[] list = {"  租賃地圖","  租賃站列表","  體能計算","  使用說明"};
+    private ArrayAdapter<String> listAdapter;
+    Integer[] imageId = {
+            R.drawable.ic_place_black_24dp,
+            R.drawable.ic_directions_bike_black_24dp,
+            R.drawable.ic_timer_black_48dp,
+            R.drawable.ic_event_note_black_24dp,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CustomList adapter = new
+                CustomList(MainActivity.this, list, imageId);
+        listView=(ListView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "You Clicked at " +list[+ position], Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
+
 }

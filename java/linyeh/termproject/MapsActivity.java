ec996c449a1572a3fafd55f7ae2efa4ce7e1bc70
EricYeh <ncy9371@hotmail.com>
2016-12-13 18:11:37 +0800

    161213 18:11

--- java/linyeh/termproject/MapsActivity.java
+++ java/linyeh/termproject/MapsActivity.java
@@ -7,6 +7,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -40,7 +42,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(24.969919, 121.266497);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        BitmapDescriptor icon= BitmapDescriptorFactory.fromResource(R.drawable.ic_place_black_24dp);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("YZU").snippet("CSE\nIC").icon(icon));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setInfoWindowAdapter(new MapsActivity_InfoWindowAdapter(MapsActivity.this)); 
     }
 }
---
+++ java/linyeh/termproject/MapsActivity_InfoWindowAdapter.java
@@ -0,0 +1,32 @@
+package linyeh.termproject;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by Eric on 2016/12/13.
+ */
+
+public class MapsActivity_InfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+    private Activity context;
+    public MapsActivity_InfoWindowAdapter(Activity in_context){
+        context = in_context;
+    }
+    @Override
+    public View getInfoWindow(Marker marker){
+        return null;
+    }
+    @Override
+    public View getInfoContents(Marker marker){
+        View view = context.getLayoutInflater().inflate(R.layout.activity_maps_infowindow, null);
+        TextView title = (TextView) view.findViewById(R.id.title);
+        TextView snippet = (TextView) view.findViewById(R.id.snippet);
+        title.setText(marker.getTitle());
+        snippet.setText(marker.getSnippet());
+        return view;
+    }
+}
